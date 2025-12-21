5c11db38ccea9fa5ae068abf7bc149b077eac6e1
Zulfat Gafurzyanov <gafurzyanov.zulfat@gmail.com>
2025-12-21 18:28:09 +1000

    добавил модель и функцию post

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Task(BaseModel):
+    """Модель данных для задач."""
+
+    name: str
+    description: str | None = None
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message: Hello Zulfat"}
+@app.post("/tasks/")
+async def create_task(task: Task):
+    return task
