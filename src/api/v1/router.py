ba395eaddff648c491b679beabe435c39b1e3284
Zulfat Gafurzyanov <gafurzyanov.zulfat@gmail.com>
2026-03-14 15:01:24 +1000

    add routers initfile

---
+++ src/api/v1/router.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+from src.api.v1.rpc import router as rpc_router
+from src.api.v1.tasks import router as tasks_router
+from src.api.v1.users import router as users_router
+
+v1_router = APIRouter(prefix="/v1")
+
+v1_router.include_router(tasks_router)
+v1_router.include_router(users_router)
+v1_router.include_router(rpc_router)
